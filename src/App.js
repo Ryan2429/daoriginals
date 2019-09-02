58ce729b6d79291e62b8d6f95a02a9e542889247
Ryan2429 <aldous.ryan1@gmail.com>
2019-09-02 11:08:59 -0400

    corrected routing error with case-sensitivity

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Images from './components/Images/Images';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import About from './components/Navigation/About';
 import Contact from './components/Navigation/Contact';
 
@@ -9,14 +9,14 @@ import Contact from './components/Navigation/Contact';
 class App extends React.Component {
  render() {
     return (
-            <BrowserRouter>
+            <Router>
                 <div style={{cursor: 'pointer'}}>
                     <Navigation />
-                    <Route exact path='/home' component={Images} />
+                    <Route path='/daoriginals' component={Images} />
                     <Route exact path='/about' component={About} />
                     <Route exact path='/contact' component={Contact} />
                 </div>
-            </BrowserRouter>
+            </Router>
         )
     }
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,22 +10,22 @@ const Navigation = () => {
   return (
     <nav style={{marginBottom: '50px'}}>
       <div className="nav-wrapper" style={{fontFamily: 'Playfair Display', lineHeight: '180%'}}>
-        <Link to='/home' className="brand-logo">
+        <Link to='/daoriginals' className="brand-logo">
         <h6 className="flow-text" style={{fontSize: '35px', paddingLeft: '30px', paddingTop: '5px', fontWeight: 'bold', letterSpacing: '2px'}}>
         DawnAldousOriginals
         </h6>
         </Link>
         <a href="/#" data-activates="mobile-demo" className="button-collapse" style={{color: 'rgba(0, 181, 204, 1)'}}><i className="material-icons">menu</i></a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><Link to='/home'><h5 style={{fontSize: '28px', fontWeight: 'bold', marginTop: '16px', paddingRight: '20px'}}>Home</h5></Link></li> 
+          <li><Link to='/daoriginals'><h5 style={{fontSize: '28px', fontWeight: 'bold', marginTop: '16px', paddingRight: '20px'}}>Home</h5></Link></li> 
           <li><Link to='/about'><h5 style={{fontSize: '28px', fontWeight: 'bold', marginTop: '16px', paddingRight: '20px'}}>About</h5></Link></li>
           <li><Link to='/contact'><h5 style={{fontSize: '28px', fontWeight: 'bold', marginTop: '16px', paddingRight: '20px'}}>Contact</h5></Link></li>
         </ul>
         <ul className="side-nav" id="mobile-demo" style={{backgroundColor: 'rgba(255,210,221, 1)'}}>
-        <li><a href='/home' style={{color: 'rgba(0, 181, 204, 1)', fontWeight: 'bold', fontSize: '20px'}}><p>DawnAldousOriginals</p></a></li>
-        <li><a href='/home' style={{color: 'rgba(0, 181, 204, 1)', fontWeight: 'bold', fontSize: '15px'}}>Home</a></li>
-        <li><a href='/About' style={{color: 'rgba(0, 181, 204, 1)', fontWeight: 'bold', fontSize: '15px'}}>About</a></li>
-        <li><a href='/Contact' style={{color: 'rgba(0, 181, 204, 1)', fontWeight: 'bold', fontSize: '15px'}}>Contact</a></li>
+        <li><a href='/daoriginals' style={{color: 'rgba(0, 181, 204, 1)', fontWeight: 'bold', fontSize: '20px'}}><p>DawnAldousOriginals</p></a></li>
+        <li><a href='/daoriginals' style={{color: 'rgba(0, 181, 204, 1)', fontWeight: 'bold', fontSize: '15px'}}>Home</a></li>
+        <li><a href='/about' style={{color: 'rgba(0, 181, 204, 1)', fontWeight: 'bold', fontSize: '15px'}}>About</a></li>
+        <li><a href='/contact' style={{color: 'rgba(0, 181, 204, 1)', fontWeight: 'bold', fontSize: '15px'}}>Contact</a></li>
   </ul>
       </div>
     </nav>
