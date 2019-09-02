82b8b6d51e431310889f4a3b164fc10a768f7a68
Ryan2429 <aldous.ryan1@gmail.com>
2019-09-02 11:55:25 -0400

    changes

--- src/components/Navigation/About.js
+++ src/components/Navigation/About.js
@@ -7,9 +7,9 @@ const About = () => {
         <div style={{marginRight: '5%', marginLeft: '5%'}}>
         <img src={ Dawn } alt="Dawn Aldous" style={{maxHeight: '290px', maxWidth: '290px', borderRadius: '50%', float: 'right', marginLeft: '20px'}}/>
         <p style={{fontSize: '30px', fontFamily: 'Playfair Display', lineHeight: '180%', color: 'rgba(255,210,221, 1)', textShadow: '3px 3px rgba(0, 171, 194, 1)'}}>
-        <span className='first-letter'>D</span>awn, an artist from Tampa, FL, has been creating fashion dolls, dresses, and fun creatures for over 20 years!
+        <span className='first-letter'>D</span>awn, an artist from Tampa, FL, has been creating fashion dolls and dresses for over 20 years!
         She is an artist of many talents who spends hours meticulously painting the same eyes over and over to bring lifelike realism to her projects. 
-        Beginning with sewing and dyeing her own teddy bears and making cute outfits for her porcelain dolls eventually she entered the world of professional doll repaints.
+        Beginning with sewing her own teddy bears and making cute outfits for porcelain dolls she eventually entered the world of professional doll repaints.
         Ever since, she has worked with a variety of clients from all over the world on countless projects. Do you have a favorite character from a television show or movie?
         It doesn't matter how obscure or fantastical your interest might be, Dawn is an artist who can take your idea and sprint with it!
         <span className='underline'>If you can think it, there is a good chance that she can make it!</span>
